6b9af66c7f602a5272fc431ed9eb233c6c0472b4
rizki maulana ramdani <rizkimramdani@gmail.com>
2019-04-07 14:47:31 +0700

    PRETEST1_AKB2_10116088

---
+++ latihan1/app/src/main/java/com/maulanaarizki/latihan1/Main2Activity.java
@@ -0,0 +1,29 @@
+package com.maulanaarizki.latihan1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ///start
+
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Main2Activity.this,Main3Activity.class);
+
+                startActivity(i);
+            }
+        });
+        ////end
+    }
+}
---
+++ latihan1/app/src/main/java/com/maulanaarizki/latihan1/Main3Activity.java
@@ -0,0 +1,29 @@
+package com.maulanaarizki.latihan1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        ///start
+
+        Button button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Main3Activity.this,Main4Activity.class);
+
+                startActivity(i);
+            }
+        });
+        ////end
+    }
+}
